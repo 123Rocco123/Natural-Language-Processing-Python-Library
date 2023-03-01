0bff16135fc62cafd7e4a59f796f595b18227416
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2023-03-01 02:33:57 +0000

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+The goal of the library is that of allowing developers to quickly and easily analyze finance text files and strings. 
+
+The functions in the package make use of ptrFinance to make and save the requests.
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Science/Research',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='ptrNaturalLanguage',
+  version='0.0.1',
+  description='Natural Language Processing Library',
+  #long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',
+  author='Rocco Pio Maria Petruccio',
+  author_email='whatsappbackuprocco@gmail.com',
+  license='MIT',
+  classifiers=classifiers,
+  keywords=["Artificial Intelligence", "Natural Language Processing"],
+  packages=find_packages(),
+  install_requires=[]
+)
