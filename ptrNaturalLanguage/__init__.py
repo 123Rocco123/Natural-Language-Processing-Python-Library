d517b8f7f09a7da29a0ec085460aa9c8c2bfac90
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2023-03-01 02:34:13 +0000

    String Check Function

--- ptrNaturalLanguage/__init__.py
+++ ptrNaturalLanguage/__init__.py
@@ -1,2 +1,5 @@
 import ptrFinance
 
+# Function Used for Strings
+    # Parameters are the stock name and the string array containg the titles of the articles
+def stringCheckFunc(stockName, keyWordsArr = [], stringArr = []):
