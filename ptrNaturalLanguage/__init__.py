3756f159f19c9e860fa0a18de3990be9442bf99c
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2023-03-01 02:34:19 +0000

    Articles Table

--- ptrNaturalLanguage/__init__.py
+++ ptrNaturalLanguage/__init__.py
@@ -3,3 +3,6 @@ import ptrFinance
 # Function Used for Strings
     # Parameters are the stock name and the string array containg the titles of the articles
 def stringCheckFunc(stockName, keyWordsArr = [], stringArr = []):
+    # Array contains the titles of the stock that the user has selected
+    articleTitles = [x for x in ptrFinance.returnMostRecentArticles(stockName) if x != ""]
+
