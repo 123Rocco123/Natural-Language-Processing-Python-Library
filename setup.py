78f08c358ef92aed8d3485ef74748b88e76a3a40
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2023-03-09 03:10:18 +0000

    Metadata Update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
 
 setup(
   name='ptrNaturalLanguage',
-  version='0.0.3',
+  version='0.0.4',
   description='Natural Language Processing Library',
   #long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
   url='',
