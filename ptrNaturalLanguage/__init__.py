a66f21ded9f213dd7199743c316b6cd9531dcfc2
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2023-03-10 07:19:44 +0000

    If-Else Block
    Used in case there are no keywords found for an article

--- ptrNaturalLanguage/__init__.py
+++ ptrNaturalLanguage/__init__.py
@@ -63,7 +63,11 @@ def websiteSearch(stockName, keyWordsArr, articleLinkArr):
                     # If the paragraph contains the keyword that we're looking for, then we add it to its respective dictionary key
                     keywordDic[keywords].append(paragraphs)
 
-        articleDictionary[list(namesOfArticles.keys())[list(namesOfArticles.values()).index(x)]] = keywordDic
+        # If-Else block used to remove all articles that don't have the keywords specified. 
+        if len(keywordDic) == 0:
+            continue
+        else:
+            articleDictionary[list(namesOfArticles.keys())[list(namesOfArticles.values()).index(x)]] = keywordDic
 
     # Return dictionary with keywords
     return articleDictionary
